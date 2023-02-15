142643f948a9e4cbabadd0558e5769ff51ce7b43
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-15 18:28:58 +0100

    Update sub_scan.py

--- scanners/sub_scan.py
+++ scanners/sub_scan.py
@@ -3,9 +3,9 @@ from time import sleep
 
 def Scan(domain):
 
-    with open(f"./scans/SUBDOMAINS/{domain}.txt", "a") as f:
-        f.write("{:<15} {:<15} {:<15} \n".format("Domain", "Subdomain", "Response Code"))
-        f.write("="*45+"\n")
+    with open(f"./scans/SUBDOMAINS/{domain}.txt", "w") as f:
+        f.write("{:<15} {:<15} {:<15} {:<15}\n".format("Domain", "Subdomain", "Response Code", "Subdomain Takeover"))
+        f.write("="*66+"\n")
 
     with open("./wordlists/sub_list.txt", "r") as f:
         subdomains = f.read().splitlines()
@@ -14,8 +14,13 @@ def Scan(domain):
         try:
             url = f"https://{subdomain}.{domain}"
             response = requests.get(url)
+
             if response.status_code != 404:
-                with open(f"./scans/SUBDOMAINS/{domain}.txt", "a") as f:
-                    f.write("{:<15} {:<15} {:<15} \n".format(domain, subdomain, str(response.status_code)))
+                if "CNAME" in response.text:
+                    with open(f"./scans/SUBDOMAINS/{domain}.txt", "a") as f:
+                        f.write("{:<15} {:<15} {:<15} {:<15} \n".format(domain, subdomain, str(response.status_code), "True"))
+                else:
+                    pass
+
         except requests.exceptions.ConnectionError as e:
             continue
