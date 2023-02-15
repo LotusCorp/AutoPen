05d27e57530bcbffa5bdb656bd5a1c7ce499636f
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-15 19:07:16 +0100

    Update lfi_scan.py

--- scanners/lfi_scan.py
+++ scanners/lfi_scan.py
@@ -4,7 +4,7 @@ import requests
 def Scan(domain):
 
     with open(f"./scans/LFI/{domain}.txt", "w") as f:
-        f.write('{:<15} {:<15} {:<30} \n'.format('Domain', 'Payload', 'LFI'))
+        f.write('{:<15} {:<45} {:<15} \n'.format('Domain', 'Payload', 'LFI'))
         f.write('='*66+'\n')
 
     with open("payloads/lfi.txt", "r") as f:
@@ -14,6 +14,6 @@ def Scan(domain):
             response = requests.get(url)
             if response.status_code != 404:
                 with open(f"./scans/LFI/{domain}.txt", "a") as f:
-                    f.write("{:<15} {:<15} {:<30} \n".format(domain, payload, "True"))
+                    f.write("{:<15} {:<45} {:<15} \n".format(domain, payload, "True"))
             else:
                 pass
