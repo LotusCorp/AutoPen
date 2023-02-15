e338decefa5eacdf4d2629cce057614623a95c5f
Yazukiwi <111310817+0xYazuko@users.noreply.github.com>
2023-02-15 18:32:17 +0100

    Update lfi_scan.py

--- scanners/lfi_scan.py
+++ scanners/lfi_scan.py
@@ -4,12 +4,12 @@ import requests
 def Scan(domain):
 
     f.write('{:<15} {:<15} {:<30} \n'.format('Domain', 'Payload', 'LFI'))
-        f.write('='*66+'\n')
+    f.write('='*66+'\n')
 
     with open("./payloads/lfi.txt", "r") as f:
         for line in f:
             payload = line.strip()
-            url = target_url + payload
+            url = domain + payload
             response = requests.get(url)
             if response.status_code != 404:
                 with open(f"./scans/LFI/{domain}.txt", "a") as f:
