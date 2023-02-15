f8f46b45c4dd129c281a9a9b9d38d1b3656253d3
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-15 19:07:30 +0100

    Create sqli_scan.py

---
+++ scanners/sqli_scan.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def Scan(domain):
+
+    with open(f'./scans/SQLI/{domain}.txt', 'w') as f:
+        f.write('{:<15} {:<45} {:<15} \n'.format('Domain', 'Payload', 'SQLI'))
+        f.write('='*66+'\n')
+
+    with open("./payloads/sqli.txt", "r") as f:
+        try:
+            for line in f:
+                payload = line.strip()
+                response = requests.get(f"https://{domain}?={payload}")
+                if response.status_code != 404:
+                    with open(f"./scans/SQLI/{domain}.txt", "a") as f:
+                        f.write("{:<15} {:<45} {:<15} \n".format(domain, payload, "True"))
+                else:
+                    pass
+        except requests.exceptions.ConnectionError as e:
+            pass
