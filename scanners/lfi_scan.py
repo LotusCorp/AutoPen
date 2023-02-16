56ad7ce5a9158d543c282536e29f87b51ea08592
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-16 13:57:11 +0100

    Update lfi_scan.py

--- scanners/lfi_scan.py
+++ scanners/lfi_scan.py
@@ -2,7 +2,7 @@ import requests
 
 
 def Scan(domain):
-
+    param = input("[LOTUS] LFI Parameter: ") # ?page= for example
     with open(f"./scans/LFI/{domain}.txt", "w") as f:
         f.write('{:<15} {:<45} {:<15} \n'.format('Domain', 'Payload', 'LFI'))
         f.write('='*66+'\n')
@@ -10,8 +10,7 @@ def Scan(domain):
     with open("payloads/lfi.txt", "r") as f:
         for line in f:
             payload = line.strip()
-            url = domain + payload
-            response = requests.get(url)
+            response = requests.get(f"https://{domain}{param}{payload}")
             if response.status_code != 404:
                 with open(f"./scans/LFI/{domain}.txt", "a") as f:
                     f.write("{:<15} {:<45} {:<15} \n".format(domain, payload, "True"))
