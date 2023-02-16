5617d2d6d17cf045059cbe78ecb2e1464ee7de40
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-16 13:58:12 +0100

    Update whois_scan.py

--- scanners/whois_scan.py
+++ scanners/whois_scan.py
@@ -4,5 +4,5 @@ import whois
 def Dump(domain):
 
     whois_info = whois.whois(domain)
-    with open(f'./scans/WHOIS/{domain}', 'w') as f:
+    with open(f'./scans/WHOIS/{domain}.txt', 'w') as f:
         f.write(str(whois_info))
