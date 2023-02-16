37701982149e18f58dbc5666b89a8779ad015866
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-16 14:06:52 +0100

    Update lfi_scan.py

--- scanners/lfi_scan.py
+++ scanners/lfi_scan.py
@@ -7,11 +7,11 @@ def Scan(domain):
         f.write('{:<15} {:<45} {:<15} \n'.format('Domain', 'Payload', 'LFI'))
         f.write('='*66+'\n')
 
-    with open("payloads/lfi.txt", "r") as f:
+    with open("./payloads/lfi.txt", "r") as f:
         for line in f:
             payload = line.strip()
             response = requests.get(f"https://{domain}{param}{payload}")
-            if response.status_code != 404:
+            if "Warning: include()" in response.text:
                 with open(f"./scans/LFI/{domain}.txt", "a") as f:
                     f.write("{:<15} {:<45} {:<15} \n".format(domain, payload, "True"))
             else:
