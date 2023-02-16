a82e48ad558253c405901b7cf831c84994a81c66
Gast <123122904+0xG4st@users.noreply.github.com>
2023-02-16 13:57:34 +0100

    Update sqli_scan.py

--- scanners/sqli_scan.py
+++ scanners/sqli_scan.py
@@ -3,6 +3,7 @@ import requests
 
 def Scan(domain):
 
+    param = input("[LOTUS] SQLi Parameter: ")
     with open(f'./scans/SQLI/{domain}.txt', 'w') as f:
         f.write('{:<15} {:<45} {:<15} \n'.format('Domain', 'Payload', 'SQLI'))
         f.write('='*66+'\n')
@@ -11,7 +12,7 @@ def Scan(domain):
         try:
             for line in f:
                 payload = line.strip()
-                response = requests.get(f"https://{domain}?={payload}")
+                response = requests.get(f"https://{domain}{param}{payload}")
                 if response.status_code != 404:
                     with open(f"./scans/SQLI/{domain}.txt", "a") as f:
                         f.write("{:<15} {:<45} {:<15} \n".format(domain, payload, "True"))
